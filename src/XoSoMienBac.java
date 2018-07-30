403247206850b3cee2e2e4bd32af9a785082d573
Tien Dat <datdta95@gmail.com>
2018-07-30 10:54:49 +0700

    Hiển thị thêm các giải trong bài toán xổ số

---
+++ src/XoSoMienBac.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class XoSoMienBac {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int lottery = random.nextInt(100);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào chữ số đầu tiên của giải: ");
+        int guessDigit1 = scanner.nextInt();
+        System.out.println("Nhập vào chữ số thứ hai của giải: ");
+        int guessDigit2 = scanner.nextInt();
+        String giaiNhat= String.valueOf(lottery);
+        String giaiNhi = new StringBuffer(giaiNhat).reverse().toString();
+        System.out.println("Giải nhất: " + lottery);
+        if (lottery < 10) {
+            System.out.println("Giải nhì: 0" + giaiNhi);
+        } else {
+            System.out.println("Giải nhì: " + giaiNhi);
+        }
+        System.out.println("Giải ba: " + lottery / 10 + "\t" + lottery % 10 + "\n");
+        if (lottery / 10 == guessDigit1 && lottery % 10 == guessDigit2) {
+            System.out.println("Bạn đã trúng giải thưởng trị giá $10000 TRUMP");
+        } else if (lottery / 10 == guessDigit2 && lottery % 10 == guessDigit1) {
+            System.out.println("Bạn đã trúng giải thưởng trị giá $3000 TRUMP");
+        } else if (lottery / 10 == guessDigit1 || lottery % 10 == guessDigit1 || lottery / 10 == guessDigit1 || lottery % 10 == guessDigit2) {
+            System.out.println("Bạn đã trúng giải thưởng trị giá $1000 TRUMP");
+        } else {
+            System.out.println("Chúc bạn may mắn lần sau!");
+        }
+    }
+}
