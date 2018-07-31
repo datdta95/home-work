52ac0ba145991cfe4507d8cb08c729e20ebd1569
Tien Dat <datdta95@gmail.com>
2018-07-31 13:35:57 +0700

    Giải phương trình bậc 2

---
+++ src/PhuongTrinhBac2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class PhuongTrinhBac2 {
+    public static void main(String[] args) {
+        System.out.println("\tGIẢI PHƯƠNG TRÌNH BẬC 2   0 = Ax^2 + Bx + C");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào hệ số a:");
+        float a = scanner.nextFloat();
+        System.out.println("Nhập vào hệ số b:");
+        float b = scanner.nextFloat();
+        System.out.println("Nhập vào hệ số c:");
+        float c = scanner.nextFloat();
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0) {
+                    System.out.println("Phương trình vô số nghiệm");
+                } else
+                    System.out.println("Phương trình vô nghiệm");
+            } else {
+                System.out.println("Phương trình có 1 nghiệm duy nhất x = " + (0 - c) / b);
+            }
+        } else {
+            double delta = Math.pow(b, 2) - 4 * a * c;
+            if (delta < 0) {
+                System.out.println("Phương trình vô nghiệm");
+            } else if (delta == 0) {
+                System.out.println("Phương trình có nghiệm kép x1 = x2 = " + (-b) / (2 * a));
+            } else {
+                System.out.println("Phương trình có 2 nghiệm phân biệt: ");
+                System.out.println("x1 = " + ((-b) + Math.sqrt(delta)) / (2 * a));
+                System.out.println("x2 = " + ((-b) - Math.sqrt(delta)) / (2 * a));
+            }
+
+        }
+    }
+}
