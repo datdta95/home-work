f57bbd96151d01416b8efc988f7851ceb697031b
Tien Dat <datdta95@gmail.com>
2018-07-31 15:22:32 +0700

    Tìm UCLN

---
+++ src/UocChungLonNhat.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class UocChungLonNhat {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào số a:");
+        int a = scanner.nextInt();
+        System.out.println("Nhập vào số b:");
+        int b = scanner.nextInt();
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if (a == 0 && b == 0) {
+            System.out.println("Không có UCLN");
+        } else if (a == 0 && b != 0) {
+            System.out.println("UCLN là: " + b);
+        } else if (a != 0 && b == 0) {
+            System.out.println("UCLN là: " + a);
+        } else {
+            while (a != b) {
+                if (a < b) {
+                    b = b - a;
+                } else {
+                    a = a - b;
+                }
+            }
+            System.out.println("UCLN là : " + a);
+        }
+
+    }
+}
