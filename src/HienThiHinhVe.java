2740d53c0778d2a4cd5fcaf14a712d7d1732f928
Tien Dat <datdta95@gmail.com>
2018-08-02 16:49:37 +0700

    Tái cấu trúc chương trình

--- src/HienThiHinhVe.java
+++ src/HienThiHinhVe.java
@@ -7,8 +7,8 @@ public class HienThiHinhVe {
         System.out.println("\tHIỂN THỊ HÌNH VẼ");
         System.out.println("Menu");
         System.out.println("1. Vẽ hình chữ nhật");
-        System.out.println("2. Draw the square");
-        System.out.println("3. Draw the rectangle");
+        System.out.println("2. Vẽ tam giác vuông cân");
+        System.out.println("3. Vẽ tam giác cân");
         System.out.println("0. Exit");
         System.out.println("Enter your choice:");
 
@@ -32,10 +32,8 @@ public class HienThiHinhVe {
                     }
                     break;
                 case 2:
-                    System.out.println("Tam giác vuông");
-                    System.out.println("Nhập vào cạnh góc vuông thứ nhất");
-                    width = scanner.nextInt();
-                    System.out.println("Nhập vào cạnh góc vuông thứ hai");
+                    System.out.println("Tam giác vuông cân");
+                    System.out.println("Nhập vào độ dài cạnh góc vuông");
                     height = scanner.nextInt();
                     for (int i = 0; i < height; i++) {
                         for (int j = 0; j <= i; j++) {
@@ -44,9 +42,25 @@ public class HienThiHinhVe {
                         System.out.println();
                     }
                     break;
+                case 3:
+                    System.out.println("Tam giác cân");
+                    System.out.println("Nhập vào độ dài cạnh đáy của tam giác cân:");
+                    int c = scanner.nextInt();
+                    for (int i = 1; i <= (c + 1) / 2; i++) {
+                        for (int j = 1; j <= c; j++) {
+                            if (j > (c + 1) / 2 - i && j < (c + 1) / 2 + i) {
+                                System.out.print("*");
+                            } else {
+                                System.out.print(" ");
+                            }
+                        }
+                        System.out.println("");
+                    }
+                    break;
                 default:
                     System.out.println("NO CHOICE!");
             }
         }
     }
 }
+
