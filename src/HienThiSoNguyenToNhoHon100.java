ae394f3682f1987c279752c94989770c2eefb26f
Tien Dat <datdta95@gmail.com>
2018-08-02 10:25:44 +0700

    Hiển thị các số nguyên tố nhỏ hơn 100

---
+++ src/HienThiSoNguyenToNhoHon100.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class HienThiSoNguyenToNhoHon100 {
+    public static void main(String[] args) {
+        System.out.println("Các số nguyên tố nhỏ hơn 100 là:");
+        for (int i = 2; i < 100; i++) {
+            boolean check = true;
+            for (int count =2;count <= Math.sqrt(i);count++) {
+                if (i % count == 0) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
