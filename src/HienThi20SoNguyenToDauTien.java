0f480b850014965e3e835a9de15db886ca5b179f
Tien Dat <datdta95@gmail.com>
2018-08-02 10:51:48 +0700

    Hiển thị 20 số nguyên tố đầu tiên

---
+++ src/HienThi20SoNguyenToDauTien.java
@@ -0,0 +1,22 @@
+public class HienThi20SoNguyenToDauTien {
+    public static void main(String[] args) {
+        System.out.println("20 số nguyên tố đầu tiên là:");
+        int index = 0;
+        for (int i = 2; i < 200; i++) {
+            boolean check = true;
+            for (int count = 2; count <= Math.sqrt(i); count++) {
+                if (i % count == 0) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) {
+                index = index + 1;
+                if (index <= 20) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
+
