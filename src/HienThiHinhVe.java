6fb68aadf869fa4a97470d34461a81de089bdd37
Tien Dat <datdta95@gmail.com>
2018-08-03 11:53:43 +0700

    Hoàn thành hiển thị tam giác vuông cân với góc vuông ở 4 góc

--- src/HienThiHinhVe.java
+++ src/HienThiHinhVe.java
@@ -7,8 +7,11 @@ public class HienThiHinhVe {
         System.out.println("\tHIỂN THỊ HÌNH VẼ");
         System.out.println("Menu");
         System.out.println("1. Vẽ hình chữ nhật");
-        System.out.println("2. Vẽ tam giác vuông cân");
-        System.out.println("3. Vẽ tam giác cân");
+        System.out.println("2. Vẽ tam giác vuông cân có góc vuông ở vị trí bottom-left");
+        System.out.println("3. Vẽ tam giác vuông cân có góc vuông ở vị trí top-right");
+        System.out.println("4. Vẽ tam giác vuông cân có góc vuông ở vị trí top-left");
+        System.out.println("5. Vẽ tam giác vuông cân có góc vuông ở vị trí bottom-right");
+        System.out.println("6. Vẽ tam giác cân");
         System.out.println("0. Exit");
         System.out.println("Enter your choice:");
 
@@ -32,7 +35,7 @@ public class HienThiHinhVe {
                     }
                     break;
                 case 2:
-                    System.out.println("Tam giác vuông cân");
+                    System.out.println("Tam giác vuông cân có góc vuông ở vị trí bottom-left");
                     System.out.println("Nhập vào độ dài cạnh góc vuông");
                     height = scanner.nextInt();
                     for (int i = 0; i < height; i++) {
@@ -67,6 +70,21 @@ public class HienThiHinhVe {
                         System.out.println();
                     }
 
+                    break;
+                case 5:
+                    System.out.println("Tam giác vuông cân có góc vuông ở vị trí bottom-right");
+                    System.out.println("Nhập vào độ dài cạnh góc vuông");
+                    height = scanner.nextInt();
+                    for (int i = 1; i <= height; i++) {
+                        for (int j = 1; j <= height; j++) {
+                            if (j >= height - i + 1) {
+                                System.out.print("* ");
+                            } else {
+                                System.out.print("  ");
+                            }
+                        }
+                        System.out.println();
+                    }
                     break;
                 case 6:
                     System.out.println("Tam giác cân");
